f0c24266a0493a780fa1ea2254f062eff5b740ed
L. W. Miller <lmiller81694@gmail.com>
2021-03-08 00:37:56 +0000

    Tidy up

--- index.js
+++ index.js
@@ -10,83 +10,84 @@
  */
 
 function getAscii() {
-  let resBox = document.getElementById("res")
-  let radius = document.getElementById("userInput").value
+  const resBox = document.getElementById("res");
+  const radius = document.getElementById("userInput").value;
 
   if (radius === "") {
-    resBox.innerHTML = "Result will be shown here"
-    return
+    resBox.innerHTML = "Result will be shown here";
+    return;
   }
 
-  let grid = getArr(radius)
-  resText = toText(grid)
-  resBox.innerHTML = resText
+  const grid = getArr(radius);
+  resText = toText(grid);
+  resBox.innerHTML = resText;
 
   return false;
 }
 
+/**
+ * @param {number} radius the radius of the circle to draw
+ * @returns {(number | boolean)[]} an array with a circle filled. The left and upper edges will be padded with 0s
+ */
 function getArr(radius) {
-  let diameter = 2 * radius
-  let width = (diameter % 2 == 0 ? diameter + 1 : diameter + 2)
-  let midGridIndex = (width / 2) + 0.5
-  let yArr = []
+  const diameter = 2 * radius;
+  const width = diameter % 2 === 0 ? diameter + 1 : diameter + 2;
+  const midGridIndex = width / 2 + 0.5;
+  const yArr = [];
 
   for (let y = 0; y <= width; y++) {
-    let xArr = []
+    const xArr = [];
 
     for (let x = 0; x <= width; x++) {
-      if (x === 0) {
-        xArr.push(0)
-      } else if (y === 0) {
-        xArr.push(0)
+      if (x === 0 || y === 0) {
+        xArr.push(0);
+        continue;
+      }
+
+      const offsetX = Math.abs(x - midGridIndex);
+      const offsetY = Math.abs(y - midGridIndex);
+      const dist = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2));
+
+      const smoothed = document.getElementById("smoothedCheck").checked
+        ? x !== 1 && x !== width && y !== 1 && y !== width
+        : true;
+
+      if (smoothed && dist <= radius) {
+        xArr.push(true);
       } else {
-        let offsetX = Math.abs(x - midGridIndex)
-        let offsetY = Math.abs(y - midGridIndex)
-        let dist = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2))
-
-
-        var test = document.getElementById("smoothedCheck").checked
-          ? x !== 1 && x !== width && y !== 1 && y !== width
-          : true
-
-        if (test && dist <= radius) {
-          xArr.push(true)
-        } else {
-          xArr.push(false)
-        }
+        xArr.push(false);
       }
     }
-    yArr.push(xArr)
+    yArr.push(xArr);
   }
-  return yArr
+
+  return yArr;
 }
 
 function toText(grid) {
-  let res = ""
+  let res = "";
 
   grid.forEach((y, yIdx) => {
     y.forEach((x, xIdx) => {
-
-      if (typeof (x) === typeof (0)) {
-        // Coordinates
-
+      // Print Coordinates if x is a number
+      if (typeof x === "number") {
         if (yIdx === 0) {
           // X-Axis
-          res += xIdx < 10 ? " " + xIdx : xIdx
+          res += xIdx < 10 ? " " + xIdx : xIdx;
         } else if (xIdx === 0) {
           // Y-Axis
-          res += yIdx < 10 ? " " + yIdx : yIdx
+          res += yIdx < 10 ? " " + yIdx : yIdx;
         }
-
       } else {
         if (x) {
-          res += "\u2588\u2588"
+          res += "\u2588\u2588";
         } else {
-          res += "\u2591\u2591"
+          res += "\u2591\u2591";
         }
       }
-    })
-    res += "\n"
-  })
+    });
+    res += "\n";
+  });
+
   return res;
 }
